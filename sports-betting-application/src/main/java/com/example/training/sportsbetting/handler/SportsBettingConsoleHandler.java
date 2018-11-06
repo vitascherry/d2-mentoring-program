840b8e677638961a62fcd2179b872d835ca7ecfd
vitascherry <vitas.cherry@gmail.com>
2018-11-06 03:38:11 +0200

    Refactored some code

--- sports-betting-application/src/main/java/com/example/training/sportsbetting/handler/SportsBettingConsoleHandler.java
+++ sports-betting-application/src/main/java/com/example/training/sportsbetting/handler/SportsBettingConsoleHandler.java
@@ -4,35 +4,26 @@ import com.example.training.common.handler.ConsoleHandler;
 import com.example.training.sportsbetting.domain.Bet;
 import com.example.training.sportsbetting.domain.BetType;
 import com.example.training.sportsbetting.domain.Outcome;
-import com.example.training.sportsbetting.domain.OutcomeOdd;
 import com.example.training.sportsbetting.domain.SportEvent;
-import com.example.training.sportsbetting.domain.SportEventType;
 import com.example.training.sportsbetting.service.SportsBettingService;
 import lombok.AllArgsConstructor;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
-import java.text.ParseException;
-import java.util.Currency;
 import java.util.List;
-import java.util.Optional;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
+import static com.example.training.common.util.CurrencyUtils.parseCurrencyCode;
+import static com.example.training.common.util.ExceptionUtils.unchecked;
+import static com.example.training.sportsbetting.util.OddUtils.getLatestOdd;
 import static java.math.BigDecimal.ZERO;
-import static java.util.Arrays.stream;
-import static java.util.Comparator.comparing;
-import static java.util.stream.Collectors.joining;
-import static java.util.stream.Collectors.toList;
+import static java.util.Collections.emptyList;
+import static java.util.Currency.getInstance;
 
 @AllArgsConstructor
 public class SportsBettingConsoleHandler extends ConsoleHandler {
-
-    private static final String SPORT_EVENT_TYPE_REGEX = stream(SportEventType.values()).map(Enum::name).collect(joining("|"));
-    private static final Pattern SPORT_EVENT_TYPE_PATTERN = Pattern.compile(SPORT_EVENT_TYPE_REGEX);
-    private static final String BET_TYPE_REGEX = stream(BetType.values()).map(Enum::name).collect(joining("|"));
-    private static final Pattern BET_TYPE_PATTERN = Pattern.compile(BET_TYPE_REGEX);
-    private static final Pattern BET_PRICE_PATTERN = Pattern.compile("(?:[\\d\\s]*)([A-Za-z]{3})");
 
     private final SportsBettingService sportsBettingService;
     private final DecimalFormat decimalFormatter;
@@ -46,27 +37,24 @@ public class SportsBettingConsoleHandler extends ConsoleHandler {
     }
 
     public void printAllBetsWithPossibleOutcomes() {
+        printWithLineBreak("Listing all bets with possible outcomes...");
         decimalFormatter.applyPattern("###,###.##");
         decimalFormatter.setGroupingUsed(false);
 
-        printWithLineBreak("Listing all bets with possible outcomes...");
-        for (SportEvent sportEvent : sportsBettingService.getSportEvents()) {
+        sportsBettingService.getSportEvents().forEach(sportEvent -> {
             printWithLineBreak("#%s. %s", sportEvent.getIdentifier().getId(), sportEvent.getTitle());
-            for (Bet bet : sportEvent.getBets()) {
+
+            sportEvent.getBets().forEach(bet -> {
                 printWithLineBreak("    #%s. %s", bet.getIdentifier().getId(),
-                        Optional.ofNullable(bet.getDescription()).orElse("-"));
-                for (Outcome outcome : bet.getOutcomes()) {
-                    printWithLineBreak("        #%s. outcome: %s, odd: %s", outcome.getIdentifier().getId(),
-                            outcome.getValue(),
-                            outcome.getOdds()
-                                    .stream()
-                                    .max(comparing(OutcomeOdd::getFrom))
-                                    .map(OutcomeOdd::getOdd)
-                                    .map(decimalFormatter::format)
-                                    .orElse("-"));
-                }
-            }
-        }
+                        bet.getDescription() != null ? bet.getDescription() : bet.getType().name());
+
+                bet.getOutcomes().forEach(outcome ->
+                        printWithLineBreak("        #%s. outcome: %s, odd: %s", outcome.getIdentifier().getId(),
+                                outcome.getValue(), getLatestOdd(outcome.getOdds())
+                                        .map(decimalFormatter::format)
+                                        .orElse("-")));
+            });
+        });
 
         printLineBreak();
     }
@@ -76,93 +64,57 @@ public class SportsBettingConsoleHandler extends ConsoleHandler {
         printWithLineBreak("You can bet on sport events by choosing one possible outcome of a bet " +
                 "and specifying the amount you wish to wager");
 
-        List<SportEvent> sportEvents = sportsBettingService.getSportEvents();
-        while (true) {
-            String value = readWithRetry(SPORT_EVENT_TYPE_PATTERN, "Please, choose sport event type: ",
-                    String.format("You need to choose one from (%s) only!", SPORT_EVENT_TYPE_REGEX));
-            final SportEventType sportEventType = SportEventType.valueOf(value);
+        final List<SportEvent> possibleSportEvents = sportsBettingService.getSportEvents();
+        SportEvent sportEvent = readValue("Please, choose the sport's event name: ", x -> x, possibleSportEvents,
+                (events, title) -> events.stream()
+                        .filter(event -> event.getTitle().equals(title))
+                        .findFirst()
+                        .orElse(null),
+                title -> possibleSportEvents.stream()
+                        .map(SportEvent::getTitle)
+                        .anyMatch(title1 -> title1.equals(title)),
+                "Not found any sport events with title '%s'");
 
-            if (sportEvents.stream().map(SportEvent::getType).anyMatch(type -> type.equals(sportEventType))) {
-                sportEvents = sportEvents.stream()
-                        .filter(event -> event.getType().equals(sportEventType))
-                        .collect(toList());
-                break;
-            }
+        final List<Bet> possibleBets = sportEvent.getBets();
+        Bet bet = readValue("Please, choose bet type: ", BetType::valueOf, possibleBets,
+                (bets, type) -> bets.stream()
+                        .filter(bet1 -> bet1.getType().equals(type))
+                        .findFirst()
+                        .orElse(null),
+                type -> possibleBets.stream()
+                        .map(Bet::getType)
+                        .anyMatch(type1 -> type1.equals(type)),
+                "Not found any bets with bet type '%s'");
 
-            printWithLineBreak("Not found any sport events with type '%s'", value);
-        }
+        final List<Outcome> possibleOutcomes = bet.getOutcomes();
+        Outcome outcome = readValue("Please, choose outcome: ", x -> x, possibleOutcomes,
+                (outcomes, value) -> outcomes.stream()
+                        .filter(outcome1 -> outcome1.getValue().equals(value))
+                        .findFirst()
+                        .orElse(null),
+                value -> possibleOutcomes.stream()
+                        .map(Outcome::getValue)
+                        .anyMatch(value1 -> value1.equals(value)),
+                "Not found any outcomes with value '%s'");
 
-        while (true) {
-            final String value = readNextLine("Please, specify the name of event: ");
-
-            if (sportEvents.stream().map(SportEvent::getTitle).anyMatch(title -> title.equalsIgnoreCase(value))) {
-                sportEvents = sportEvents.stream()
-                        .filter(event -> event.getTitle().equals(value))
-                        .collect(toList());
-                break;
-            }
-
-            printWithLineBreak("Not found any sport events with title '%s'", value);
-        }
-
-        SportEvent sportEvent = sportEvents.get(0);
-        List<Bet> bets = sportEvent.getBets();
-        while (true) {
-            String value = readWithRetry(BET_TYPE_PATTERN, "Please, choose bet type: ",
-                    String.format("You need to choose one from (%s) only!", BET_TYPE_REGEX));
-            final BetType betType = BetType.valueOf(value);
-
-            if (bets.stream().map(Bet::getType).anyMatch(type -> type.equals(betType))) {
-                bets = bets.stream()
-                        .filter(bet -> bet.getType().equals(betType))
-                        .collect(toList());
-                break;
-            }
-
-            printWithLineBreak("Not found any bets with bet type '%s'", value);
-        }
-
-        Bet bet = bets.get(0);
-        List<Outcome> outcomes = bet.getOutcomes();
-        while (true) {
-            final String value = readNextLine("Please, choose outcome: ");
-
-            if (outcomes.stream().map(Outcome::getValue).anyMatch(outcome -> outcome.equalsIgnoreCase(value))) {
-                outcomes = outcomes.stream()
-                        .filter(outcome -> outcome.getValue().equals(value))
-                        .collect(toList());
-                break;
-            }
-
-            printWithLineBreak("Not found any outcomes with value '%s'", value);
-        }
-
-        decimalFormatter.applyPattern("###,###.##");
-        Outcome outcome = outcomes.get(0);
-        BigDecimal odd = outcome.getOdds()
-                .stream()
-                .max(comparing(OutcomeOdd::getFrom))
-                .map(OutcomeOdd::getOdd)
+        BigDecimal odd = getLatestOdd(outcome.getOdds())
                 .map(BigDecimal::valueOf)
                 .orElse(ZERO);
+        decimalFormatter.applyPattern("###,###.##");
         printWithLineBreak("The odd is %s", decimalFormatter.format(odd));
 
-        String currencyCode;
-        BigDecimal betPrice;
-        while (true) {
-            String value = readNextLine("How much do you want to bet? ");
-            try {
-                currencyCode = parseCurrencyCode(value);
-                betPrice = (BigDecimal) decimalFormatter.parse(value);
-                decimalFormatter.setCurrency(Currency.getInstance(currencyCode));
-                decimalFormatter.applyPattern("###,###.## " + currencyCode);
-                break;
-            } catch (ParseException e) {
-                printWithLineBreak("Not found currency code in text '%s'. Please, choose another one!", value);
-            }
-
-            printWithLineBreak("Please, specify an amount with a 3-letter currency code!");
-        }
+        BigDecimal betPrice = readValue("How much do you want to bet? ",
+                unchecked((String text) -> (BigDecimal) decimalFormatter.parse(text))
+                        .compose(unchecked((String text) -> {
+                            String currencyCode = parseCurrencyCode(text);
+                            decimalFormatter.setCurrency(getInstance(currencyCode));
+                            decimalFormatter.applyPattern("###,###.## " + currencyCode);
+                            return text;
+                        })),
+                emptyList(),
+                (emptyList, value) -> value,
+                value -> true,
+                "Please, specify an amount with a 3-letter currency code!");
 
         List<Outcome> sportEventResults = sportsBettingService.getSportEventResults(sportEvent);
         if (sportEventResults.contains(outcome)) {
@@ -174,14 +126,18 @@ public class SportsBettingConsoleHandler extends ConsoleHandler {
         printLineBreak();
     }
 
-    private String parseCurrencyCode(String text) throws ParseException {
-        Matcher currencyCodeMatcher = BET_PRICE_PATTERN.matcher(text);
-        String currencyCode;
-        if (!currencyCodeMatcher.find() || (currencyCode = currencyCodeMatcher.group(1)) == null) {
-            throw new ParseException(text, currencyCodeMatcher.start(1));
-        }
+    private <T, R> T readValue(String message, Function<String, R> parser, List<T> data, BiFunction<List<T>, R, T> filter,
+                               Predicate<R> predicate, String errorMessage) {
+        while (true) {
+            String text = readNextLine(message);
+            final R value = parser.apply(text);
 
-        return currencyCode;
+            if (predicate.test(value)) {
+                return filter.apply(data, value);
+            }
+
+            printWithLineBreak(errorMessage, text);
+        }
     }
 
     public void printGoodbye() {
---
+++ sports-betting-application/src/main/java/com/example/training/sportsbetting/util/OddUtils.java
@@ -0,0 +1,18 @@
+package com.example.training.sportsbetting.util;
+
+import com.example.training.sportsbetting.domain.OutcomeOdd;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Comparator.comparing;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class OddUtils {
+
+    public static Optional<Double> getLatestOdd(List<OutcomeOdd> odds) {
+        return odds.stream().max(comparing(OutcomeOdd::getFrom)).map(OutcomeOdd::getOdd);
+    }
+}
