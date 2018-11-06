a8c1031c5b59f43f4f8f8cabc5e1b862f6f2b064
vitascherry <vitas.cherry@gmail.com>
2018-11-06 03:37:56 +0200

    Refactored some code

--- toto-application/src/main/java/com/example/training/toto/mapper/PriceDeserializer.java
+++ toto-application/src/main/java/com/example/training/toto/mapper/PriceDeserializer.java
@@ -12,16 +12,14 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.text.ParseException;
 import java.util.Currency;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
+import static com.example.training.common.util.CurrencyUtils.parseCurrencyCode;
 import static java.math.BigDecimal.ZERO;
 
 @Log4j2
 @RequiredArgsConstructor
 public class PriceDeserializer extends JsonDeserializer<Price> {
 
-    private static final Pattern CURRENCY_CODE_PATTERN = Pattern.compile("(?:[\\d\\s]*)([A-Za-z]{3})");
     private final DecimalFormat decimalFormatter;
     private Price zeroPrice = null;
 
@@ -42,17 +40,6 @@ public class PriceDeserializer extends JsonDeserializer<Price> {
             log.error("Error occurred while parsing Price from text '{}'. ErrorMessage: ", text, e);
             return zeroPrice;
         }
-    }
-
-    private String parseCurrencyCode(String text) throws ParseException {
-        Matcher currencyCodeMatcher = CURRENCY_CODE_PATTERN.matcher(text);
-        String currencyCode;
-        if (!currencyCodeMatcher.find() || (currencyCode = currencyCodeMatcher.group(1)) == null) {
-            log.warn("Not found currency code in text '{}'", text);
-            throw new ParseException(text, currencyCodeMatcher.start(1));
-        }
-
-        return currencyCode;
     }
 
     private void init(Currency currency) {
