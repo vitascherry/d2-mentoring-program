d0a6977178448301d4816b520be46a2d86514d81
vitascherry <vitas.cherry@gmail.com>
2019-04-25 15:06:02 +0300

    Refactor config resources

--- config/src/main/resources/copy.bat
+++ config/src/main/resources/copy.bat
@@ -1,5 +1,5 @@
 set PROJECT_DIR=%1
 
 xcopy %PROJECT_DIR%\env.properties %CATALINA_HOME%\bin
-xcopy %PROJECT_DIR%\setenv.sh %CATALINA_HOME%\bin
-xcopy %PROJECT_DIR%\setenv.bat %CATALINA_HOME%\bin
+xcopy setenv.sh %CATALINA_HOME%\bin
+xcopy setenv.bat %CATALINA_HOME%\bin
