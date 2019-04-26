4bc1ca65fda36c78b3db39c81f5fd7d534eb15ed
vitascherry <vitas.cherry@gmail.com>
2019-04-26 13:30:34 +0300

    Remove unused properties

--- exchange-service/src/main/java/com/example/training/exchangeservice/constant/ExchangeConstants.java
+++ exchange-service/src/main/java/com/example/training/exchangeservice/constant/ExchangeConstants.java
@@ -11,8 +11,4 @@ public final class ExchangeConstants {
     public static final String NBU_API_URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange";
 
     public static final String SYSTEM_CURRENCY_CODE = "UAH";
-
-    public static final int EXCHANGE_RATE_CLIENT_RETRY_LIMIT = 2;
-
-    public static final int EXCHANGE_RATE_CLIENT_MAX_WAIT_TIMEOUT_MILLIS = 1_000;
 }
