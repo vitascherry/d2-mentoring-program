3870fdafa93474d3706397b4d98f2ce50c367b56
vitascherry <vitas.cherry@gmail.com>
2019-04-26 13:33:14 +0300

    Update deploy scripts

--- config/src/main/resources/copy.bat
+++ config/src/main/resources/copy.bat
@@ -1,5 +1,20 @@
 set PROJECT_DIR=%1
+set DEFAULT_PROPERTIES=default.env.properties
+set PROPERTIES=env.properties
+set "TARGET_DIR=%CATALINA_HOME%\bin"
 
-xcopy %PROJECT_DIR%\env.properties %CATALINA_HOME%\bin
-xcopy setenv.sh %CATALINA_HOME%\bin
-xcopy setenv.bat %CATALINA_HOME%\bin
+copy /y NUL "%TARGET_DIR%\%PROPERTIES%" >NUL
+copy /y setenv.sh "%CATALINA_HOME%\bin"
+copy /y setenv.bat "%CATALINA_HOME%\bin"
+
+for /F "tokens=1* delims==" %%A in ("%PROJECT_DIR%\%PROPERTIES%") do (
+    for /F "tokens=1* delims==" %%C in ("%PROJECT_DIR%\%DEFAULT_PROPERTIES%") do (
+        if "%%A"=="%%C"
+            echo "%%A=%%B" >> "%TARGET_DIR%\%PROPERTIES%"
+        else
+            echo "%%C=%%D" >> "%TARGET_DIR%\%PROPERTIES%"
+    )
+)
+
+echo "Printing merged env.properties"
+type "%TARGET_DIR%"\"%PROPERTIES%"
--- config/src/main/resources/copy.sh
+++ config/src/main/resources/copy.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env sh
 
 export PROJECT_DIR=$1
+export DEFAULT_PROPERTIES=default.env.properties
+export PROPERTIES=env.properties
+export TARGET_DIR="$CATALINA_HOME/bin"
 
-cp setenv.sh setenv.bat $PROJECT_DIR/env.properties $CATALINA_HOME/bin
+\cp setenv.sh setenv.bat "$TARGET_DIR"
+\cp $DEFAULT_PROPERTIES "$TARGET_DIR/$PROPERTIES"
+
+if [ -f "$PROJECT_DIR/$PROPERTIES" ]
+then
+  \awk -F= '!a[$1]++' $PROPERTIES "$TARGET_DIR/$PROPERTIES"
+fi
+
+echo "Printing merged env.properties"
+cat "$TARGET_DIR/$PROPERTIES"
