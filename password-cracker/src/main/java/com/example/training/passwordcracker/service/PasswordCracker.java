be3b071029ff476b8e8b940cfcddd3fa22a94351
vitascherry <vitas.cherry@gmail.com>
2019-05-03 12:47:46 +0300

    Refactor password-cracker module

--- password-cracker/src/main/java/com/example/training/passwordcracker/service/PasswordCracker.java
+++ password-cracker/src/main/java/com/example/training/passwordcracker/service/PasswordCracker.java
@@ -2,9 +2,14 @@ package com.example.training.passwordcracker.service;
 
 import lombok.AllArgsConstructor;
 
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeoutException;
 
 import static com.example.training.passwordcracker.constant.HashCalculatorConstants.PASSWORD_CALCULATION_TIMEOUT_MIN;
+import static java.util.Objects.requireNonNull;
 import static java.util.concurrent.TimeUnit.MINUTES;
 
 @AllArgsConstructor
@@ -12,7 +17,9 @@ public class PasswordCracker {
 
     private final HashCalculator hashCalculator;
 
-    public String crack(String hash) throws InterruptedException, TimeoutException, ExecutionException {
+    public String crack(final String hash) throws InterruptedException, TimeoutException, ExecutionException {
+        requireNonNull(hash, "hash must not be null");
+
         ExecutorService executor = Executors.newWorkStealingPool();
         Future<String> passwordHolder = executor.submit(() -> {
             String password = "";
