38658b560565742464855ce6796a57360fb12af4
vitascherry <vitas.cherry@gmail.com>
2019-12-28 02:01:49 +0200

    Clean-up some code

--- graphy/src/main/java/com/example/training/graphy/factory/SingletonFactory.java
+++ graphy/src/main/java/com/example/training/graphy/factory/SingletonFactory.java
@@ -1,15 +1,31 @@
 package com.example.training.graphy.factory;
 
+import com.example.training.graphy.linker.Linker;
 import lombok.AccessLevel;
-import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-@NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class SingletonFactory {
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public final class SingletonFactory<T> implements Factory<T> {
+
+    private final Factory<T> delegate;
+    private transient T value;
+
+    @Override
+    public T get(Linker linker) {
+        if (value == null) {
+            synchronized (this) {
+                if (value == null) {
+                    return value = delegate.get(linker);
+                }
+            }
+        }
+        return value;
+    }
 
     public static <T> Factory<T> of(Factory<T> delegate) {
         log.debug("Memoizing {}", delegate);
-        return new MemoizingFactory<>(delegate);
+        return new SingletonFactory<>(delegate);
     }
 }
